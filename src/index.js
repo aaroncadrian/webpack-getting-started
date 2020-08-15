b363b8138a52c6300d9e1673f215709290c1fb96
Aaron Adrian <aaroncadrian@gmail.com>
2020-08-15 13:56:17 -0500

    Format with prettier

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
   return element;
 }
 
